1607df76c3f6f1545beff43ca89627be07470c69
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-03-13 13:16:30 +0700

    setup code

---
+++ src/pages/users/homePage/homePage.js
@@ -0,0 +1,13 @@
+import { memo } from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Header</h1>
+      <h1>Body</h1>
+      <h1>footer</h1>
+    </>
+  );
+};
+
+export default memo(HomePage);
